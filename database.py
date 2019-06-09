79dcae9bb32669bee293b664f2d0366526dacc84
unknown <victorcavalhere@hotmail.com>
2019-06-09 19:37:15 -0300

    ataualizando

--- database.py
+++ database.py
@@ -4,11 +4,11 @@ class DB:
     def __init__(self):
         self.conn = sqlite3.connect("pi.db")
         self.cur = self.conn.cursor()
-        self.cur.execute("CREATE TABLE IF NOT EXISTS cliente (id INTEGER PRIMARY KEY, data DATETIME DEFAULT CURRENT_TIMESTAMP)")
-        self.cur.execute("CREATE TABLE IF NOT EXISTS maquina (id INTEGER PRIMARY KEY, num_serie INTEGER, hist_op DATETIME DEFAULT CURRENT_TIMESTAMP, id_operador_fk INTEGER, id_cliente_fk INTEGER, FOREIGN KEY(id_operador_fk) REFERENCES operador(id),FOREIGN KEY(id_cliente_fk) REFERENCES cliente(id))")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS cliente (id INTEGER PRIMARY KEY,nome TEXT,data DATETIME DEFAULT CURRENT_TIMESTAMP)")
         self.cur.execute("CREATE TABLE IF NOT EXISTS operador (id INTEGER PRIMARY KEY,nome TEXT,hist_manutencao  DATETIME DEFAULT CURRENT_TIMESTAMP)")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS maquina (id INTEGER PRIMARY KEY, num_serie INTEGER, hist_op DATETIME DEFAULT CURRENT_TIMESTAMP, id_operador_fk INTEGER, id_cliente_fk INTEGER, FOREIGN KEY(id_operador_fk) REFERENCES operador(id),FOREIGN KEY(id_cliente_fk) REFERENCES cliente(id))")
         self.cur.execute("CREATE TABLE IF NOT EXISTS regiao (id INTEGER PRIMARY KEY,hist_atuacao DATETIME DEFAULT CURRENT_TIMESTAMP)")
-        self.cur.execute("CREATE TABLE IF NOT EXISTS capsula (quantidade INTEGER,hist_atualizacao DATETIME DEFAULT CURRENT_TIMESTAMP)")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS capsula (quantidade INTEGER,hist_atualizacao DATETIME DEFAULT CURRENT_TIMESTAMP, id_maquina_fk INTEGER,FOREIGN KEY(id_maquina_fk) REFERENCES maquina(id))")
 
 
         self.conn.commit()
@@ -16,61 +16,65 @@ class DB:
     def __del__(self):
         self.conn.close()
 
-    def view(self):
-        self.cur.execute("SELECT * FROM paciente")
+    def view(self,nome):
+        self.cur.execute("SELECT nome FROM operador WHERE nome=?)",(nome))
         rows = self.cur.fetchall()
         return rows
 
-    def insert(self, nome, telefone,cpf ,endereco,data_nasc,data_entrada ):
-        self.cur.execute("INSERT INTO paciente VALUES (NULL,?,?,?,?,?,?)", (nome,telefone,cpf,endereco,data_nasc,data_entrada))
-        self.conn.commit()
-        self.view()
+    def view_capsula(self,num):
+        self.cur.execute("SELECT quantidade FROM capsula WHERE id_maquina_fk=?",(num,))
+        rows = self.cur.fetchone()
+        return rows
 
-    def insert_temp(self,temp,intervalo,condicao,id_paciente):
-        self.cur.execute("INSERT INTO temperatura VALUES (NULL,?,?,?,?)", (temp,intervalo,condicao,id_paciente))
+    def view_cliente(self):
+        self.cur.execute("SELECT nome FROM cliente")
+        rows = self.cur.fetchall()
+        return rows
+
+    def insert_cliente(self,nome,data):
+        self.cur.execute("INSERT INTO cliente VALUES (NULL,?,?)", (nome,data))
         self.conn.commit()
-        self.view()
+
+    def insert_operador(self, nome,data):
+        self.cur.execute("INSERT INTO operador VALUES (NULL,?,?)", (nome,data))
+        self.conn.commit()
+
+
+    def insert_maquina(self,num,data,id_op,id_cl):
+        self.cur.execute("INSERT INTO maquina VALUES (NULL,?,?,?,?)", (num,data,id_op,id_cl))
+        self.conn.commit()
+
+
+    def insert_capsula(self,qtd,data,id_maq):
+        self.cur.execute("INSERT INTO capsula VALUES (?,?,?)", (qtd,data,id_maq))
+        self.conn.commit()
+
     
-    def insert_pressao(self,pressao,intervalo,condicao,id_paciente):
-        self.cur.execute("INSERT INTO pressao VALUES (NULL,?,?,?,?)", (pressao,intervalo,condicao,id_paciente))
-        self.conn.commit()
-        self.view()
+    def update_capsula(self, qtd, data,id_maq):
+        self.cur.execute("UPDATE capsula SET quantidade=? WHERE hist_atualizacao=? and id_maquina_fk=?", (qtd,data,id_maq))
 
-    def insert_batimento(self,batimento,intervalo,condicao,id_paciente):
-        self.cur.execute("INSERT INTO batimento VALUES (NULL,?,?,?,?)", (batimento,intervalo,condicao,id_paciente))
-        self.conn.commit()
-        self.view()
-
-    def update(self, id, nome, telefone, cpf):
-        self.cur.execute("UPDATE paciente SET nome=?, telefone=?, cpf=? WHERE id=?", (nome,telefone,cpf,id))
-        self.view()
 
     def delete(self, id):
         self.cur.execute("DELETE FROM paciente WHERE id=?", (id,))
         self.conn.commit()
-        self.view()
+
 
     def search(self, nome="", telefone="", cpf=""):
         self.cur.execute("SELECT * FROM paciente WHERE nome=? OR telefone=? OR cpf=?", (nome,telefone,cpf))
         rows = self.cur.fetchall()
         return rows
     
-    def select_id(self, nome="", telefone="", cpf=""):
-        self.cur.execute("SELECT id FROM paciente WHERE nome=? OR telefone=? OR cpf=?", (nome,telefone,cpf))
+    def select_id_cliente(self,nome):
+        self.cur.execute("SELECT id FROM cliente WHERE nome=?", (nome,))
         rows = self.cur.fetchone()
         return rows
 
-    def view_temp(self,id_):
-        self.cur.execute("SELECT temperatura FROM temperatura WHERE id_paciente = ?",(id_))
-        rows = self.cur.fetchall()
+    def select_id_operador(self,nome):
+        self.cur.execute("SELECT id,nome FROM operador WHERE nome=?", (nome,))
+        rows = self.cur.fetchone()
         return rows
 
-    def view_pressao(self,id_):
-        self.cur.execute("SELECT pressao FROM pressao WHERE id_paciente = ?",(id_))
-        rows = self.cur.fetchall()
-        return rows
-
-    def view_batimento(self,id_):
-        self.cur.execute("SELECT batimento FROM batimento WHERE id_paciente = ?",(id_))
-        rows = self.cur.fetchall()
+    def select_id_maquina(self,numero):
+        self.cur.execute("SELECT id FROM maquina WHERE num_serie=?", (numero,))
+        rows = self.cur.fetchone()
         return rows
--- main.py
+++ main.py
@@ -3,155 +3,29 @@ from database import DB
 from tkinter import *
 from tkinter import messagebox
 from random import randint
+from datetime import datetime, timezone
+from threading import Lock, Thread
 import time
 
+
 db = DB()
-
-
-
-running = False
-
-def get_selected_row(event):
-    global selected_tuple
-    index = list1.curselection()[0]
-    selected_tuple = list1.get(index)
-    e1.delete(0, END)
-    e1.insert(END, selected_tuple[1])
-    e2.delete(0, END)
-    e2.insert(END, selected_tuple[2])
-    e3.delete(0, END)
-    e3.insert(END, selected_tuple[3])
-    e14.delete(0, END)
-    e14.insert(END, selected_tuple[4])
-    e15.delete(0, END)
-    e15.insert(END, selected_tuple[5])
-    e16.delete(0, END)
-    e16.insert(END, selected_tuple[6])
-
-
-def view_command():
-    list1.delete(0, END)
-    for row in db.view():
-        list1.insert(END, row)
-
-
-def search_command():
-    list1.delete(0, END)
-    for row in db.search(title_text.get(), author_text.get(), isbn_text.get()):
-        list1.insert(END, row)
-
-
-def add_command():
-    db.insert(title_text.get(), author_text.get(), isbn_text.get(),endereco.get(),data_nasc.get(),data_entrada.get() )
-    list1.delete(0, END)
-    list1.insert(END, (title_text.get(), author_text.get(), isbn_text.get(),endereco.get(),data_nasc.get(),data_entrada.get() ))
-
-
-def delete_command():
-    db.delete(selected_tuple[0])
-
-
-def update_command():
-    db.update(selected_tuple[0], title_text.get(), author_text.get(), isbn_text.get())
-
-def view_temp():
-    list2.delete(0, END)
-    list3.delete(0, END)
-    list4.delete(0, END)
-    id_ = db.select_id(title_text.get(), author_text.get(),isbn_text.get())
-    print(id_)
-    for row in db.view_temp(id_):
-        list2.insert(END, row)
-    for row in db.view_pressao(id_):
-        list3.insert(END, row)
-    for row in db.view_batimento(id_):
-        list4.insert(END, row)
-
-def temps(x_temp,y_temp,x_pressao,y_pressao,x_batimento,y_batimento):
-    return randint(x_temp,y_temp), randint(x_pressao,y_pressao), randint(x_batimento,y_batimento)
-
-
-def start_medicao():
-    if running:
-        id_ = db.select_id(title_text.get(), author_text.get(), isbn_text.get())
-        if int(temp[0]) > temp_normal.get(): 
-            db.insert_temp(str(temp[0])+"ºC",x,temp_normal.get(),int(id_[0]))
-        if int (temp[1]) > pressao_normal.get():
-            db.insert_pressao(str(temp[1]) +"PAS",x,pressao_normal.get(),int(id_[0]))
-        if int(temp[2]) > batimento_normal.get():
-            db.insert_batimento(str(temp[2])+"FC",x,batimento_normal.get(),int(id_[0]))
-
-    window.after(100,start_medicao)
-
-def start():
-    global running
-    running = True
-
-def stop():
-    global running
-    running = False
-
-window = Tk()
-
-window.geometry("1000x400") #You want the size of the app to be 500x500
-
-
-window.title("Maquina recicladora")
-
-
-
-def on_closing():
-    dd = db
-    if messagebox.askokcancel("Quit", "Você gostaria de sair?"):
-        window.destroy()
-        del dd
-
-
-window.protocol("WM_DELETE_WINDOW", on_closing)  # handle window closing
-
-
-
-
-l1 = Label(window, text="Operador")
-l1.grid(row=0, column=0)
-
-title_text = StringVar()
-e1 = Entry(window, textvariable=title_text)
-e1.grid(row=0, column=1)
-
-list1 = Listbox(window, height=6, width=50)
-list1.grid(row=6, column=0, rowspan=6, columnspan=2)
-
-sb1 = Scrollbar(window)
-sb1.grid(row=5, column=2, rowspan=6)
-
-list1.configure(yscrollcommand=sb1.set)
-sb1.configure(command=list1.yview)
-
-list1.bind('<<ListboxSelect>>', get_selected_row)
-
-
-b1 = Button(window, text="Consultar todos", width=18, command=view_command)
-b1.grid(row=2, column=3)
-
-b2 = Button(window, text="Procurar entrada", width=18, command=search_command)
-b2.grid(row=3, column=3)
-
-b3 = Button(window, text="Adicionar entrada", width=18, command=add_command)
-b3.grid(row=4, column=3)
-
-b4 = Button(window, text="Atualizar selecionado", width=18, command=update_command)
-b4.grid(row=5, column=3)
-
-b5 = Button(window, text="Deletar selecionado", width=18, command=delete_command)
-b5.grid(row=6, column=3)
-
-b6 = Button(window, text="Fechar", width=18, command=window.destroy)
-b6.grid(row=7, column=3)
-
-
-window.after(100, start_medicao)
-window.mainloop()
-
-
-
+data = datetime.now()
+nome_cliente = input(str("Digite o nome do cliente"))
+nome_operador = input(str("Digite o nome do operador: "))
+numero_serie_maq = int(input("Digite o numero de série da maquina: "))
+db.insert_cliente(nome_cliente,data)
+db.insert_operador(nome_operador,data)
+id_cl = db.select_id_cliente(nome_cliente)[0]
+id_op = db.select_id_operador(nome_operador)[0]
+db.insert_maquina(numero_serie_maq,data,id_op,id_cl)
+num = db.select_id_maquina(numero_serie_maq)[0]
+db.insert_capsula(0,data,num)
+x=0
+while True:
+    x+=1
+    db.update_capsula(x,data,num)
+    time.sleep(1)
+    capsulas = db.view_capsula(num)[0]
+    operador = db.select_id_operador(nome_operador)[1]
+    print("Cliente: "+nome_cliente+" Operador: "+operador+" Quantiade de Capsulas: "+str(capsulas)+" Maquina: "+str(num))
+    # print(capsulas,operador)
