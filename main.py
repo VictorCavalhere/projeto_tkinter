cbe2867afa0b016cdcaa970a8a07e71a73b8d2d7
Victor <victorcavalhere@hotmail.com>
2019-06-10 22:31:31 +0100

    rasp

--- main.py
+++ main.py
@@ -6,11 +6,19 @@ from random import randint
 from datetime import datetime, timezone
 from threading import Lock, Thread
 import time
+import RPi.GPIO as GPIO
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
 
 
 db = DB()
 data = datetime.now()
-nome_cliente = input(str("Digite o nome do cliente"))
+nome_cliente = input(str("Digite o nome do cliente: "))
 nome_operador = input(str("Digite o nome do operador: "))
 numero_serie_maq = int(input("Digite o numero de série da maquina: "))
 db.insert_cliente(nome_cliente,data)
@@ -21,11 +29,15 @@ db.insert_maquina(numero_serie_maq,data,id_op,id_cl)
 num = db.select_id_maquina(numero_serie_maq)[0]
 db.insert_capsula(0,data,num)
 x=0
+print("Argudando entrada")
 while True:
-    x+=1
-    db.update_capsula(x,data,num)
-    time.sleep(1)
-    capsulas = db.view_capsula(num)[0]
-    operador = db.select_id_operador(nome_operador)[1]
-    print("Cliente: "+nome_cliente+" Operador: "+operador+" Quantiade de Capsulas: "+str(capsulas)+" Maquina: "+str(num))
-    # print(capsulas,operador)
+    if GPIO.input(23) == 1:
+       x+=1
+       db.update_capsula(x,data,num)
+       GPIO.output(18,GPIO.HIGH)
+       time.sleep(1)
+       capsulas = db.view_capsula(num)[0]
+       operador = db.select_id_operador(nome_operador)[1]
+       print("| Cliente: "+nome_cliente+"| Operador: "+operador+"| Quantiade de Capsulas: "+str(capsulas)+"| Maquina: "+str(num))
+       GPIO.output(18,GPIO.LOW)
+       print("Aguardando Entrada")
